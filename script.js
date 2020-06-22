11fef66c1303f4f0b2e56f3cc7aa02f9babb3565
Zach Johnson <Knownothingclubbers.gmail.com>
2020-06-22 08:34:48 -0400

    fixed bug when pausing

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let form = document.getElementById("myForm");
+let playButton = document.getElementById("playButton");
+let pauseButton = document.getElementById("pauseButton");
+
 let slideshow;
 
 // Holds main functionality -- submit AJAX request and start slideshow
@@ -16,11 +19,25 @@ form.addEventListener("submit", (event) => {
       })
       .then((data) => {
         pauseButton.style.opacity = "100";
-        if(slideshow) slideshow.stop();
-        Slideshow.clear();
+        if (slideshow) slideshow.clear();
         data = JSON.parse(data);
-        slideshow = new Slideshow(data)
+        slideshow = new Slideshow(data);
         slideshow.start();
       });
   })();
 });
+
+let togglePlay = document.getElementById("play_pause");
+togglePlay.addEventListener("click", playPause);
+function playPause() {
+  console.log(slideshow.playing);
+  if (slideshow.playing) {
+    slideshow.stop();
+    playButton.style.opacity = "100";
+    pauseButton.style.opacity = "0";
+  } else {
+    slideshow.start();
+    pauseButton.style.opacity = "100";
+    playButton.style.opacity = "0";
+  }
+}
--- slideshow.js
+++ slideshow.js
@@ -1,65 +1,52 @@
 class Slideshow {
-  constructor(data) {
-      this.playing = false;
+  constructor(data, timePerSlide = 5) {
+    // Slideshow Info
+    this.playing = false;
+    this.timePerSlide = timePerSlide * 1000;
 
+    // Image Setup
+    // A probably sloppily implemented linked list.
     this.StartingImage = new Image(data[0], 0);
     this.StartingImage.load();
     this.StartingImage.next = this.StartingImage;
     let currentImage = this.StartingImage;
+
     for (let i = 1; i < data.length; i++) {
       let nextImage = new Image(data[i], i);
       nextImage.load();
       currentImage.next = nextImage;
       currentImage = nextImage;
     }
+
     currentImage.next = this.StartingImage;
     this.currentImage = currentImage;
-
-    this.initializeControls();
   }
   start() {
-      this.playing = true;
-      this.advanceSlide()
-      this.timer = setInterval(() => {
-        this.advanceSlide()
-      }, 5000);  
+    this.playing = true;
+    this.advanceSlide();
+    this.timer = setInterval(() => {
+      this.advanceSlide();
+    }, this.timePerSlide);
   }
 
   stop() {
-    clearInterval(this.timer)
+    clearInterval(this.timer);
     this.playing = false;
   }
 
   advanceSlide() {
     this.currentImage.hide();
-      this.currentImage = this.currentImage.next;
-      this.currentImage.updateInfoBar();
-      this.currentImage.show();
+    this.currentImage = this.currentImage.next;
+    this.currentImage.updateInfoBar();
+    this.currentImage.show();
   }
 
-  initializeControls() {
-    let playButton = document.getElementById("playButton");
-    let pauseButton = document.getElementById("pauseButton");
-    let togglePlay = document.getElementById("play_pause");
-
-    togglePlay.addEventListener("click", () => {
-        if (this.playing) {
-        this.stop();
-          playButton.style.opacity = "100";
-          pauseButton.style.opacity = "0";
-        } else {
-          this.start();
-          pauseButton.style.opacity = "100";
-          playButton.style.opacity = "0";
-        }
-      });
-  }
-
-  static clear() {
+  clear() {
     let currentSlideShow = document.body.querySelector(".slideshow");
     while (currentSlideShow.firstChild) {
       currentSlideShow.removeChild(currentSlideShow.lastChild);
     }
+    this.stop();
   }
 }
 
@@ -70,32 +57,30 @@ class Image {
     this.title = title;
     this.source = source;
     this.date = date;
-    this.id = id
+    this.id = id;
   }
 
   load() {
-    let slideshow = document.body.querySelector(".slideshow")
+    let slideshow = document.body.querySelector(".slideshow");
     slideshow.insertAdjacentHTML(
-      // Insert the slide
+      // Insert the slide. After this refactor, I'm not sure I use the id any more.
       "beforeend",
       `<div class="slide"><img id="${this.id}" src=${this.url} alt ="${this.title}"></div>`
     );
     this.slide = slideshow.lastChild;
-
   }
 
-    show() {
-        this.slide.classList.add("active");
-    }
-    hide() {
-        this.slide.classList.remove("active");
-    }
-    updateInfoBar() {
-        document.getElementById("title").textContent = this.title;
-        document.getElementById("artist").textContent = this.artist;
-        document.getElementById("date").textContent = this.date;
-        document.getElementById("source").textContent = this.source;
-        // document.getElementById("usage").textContent = this.usage;
-    }
+  show() {
+    this.slide.classList.add("active");
   }
-
+  hide() {
+    this.slide.classList.remove("active");
+  }
+  updateInfoBar() {
+    document.getElementById("title").textContent = this.title;
+    document.getElementById("artist").textContent = this.artist;
+    document.getElementById("date").textContent = this.date;
+    document.getElementById("source").textContent = this.source;
+    // document.getElementById("usage").textContent = this.usage;
+  }
+}
